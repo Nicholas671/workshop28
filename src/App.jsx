cde64bcfb878f16f56c0473abadcae69ee7642ff
Nicholas671 <nicholas.a.crabtree@gmail.com>
2025-01-31 10:45:13 -0700

    blue and red components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-
-
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
-        <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+
+
+        <div id="navbar">
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>}></Route>
+            <Route path="/red" element={<h1>Red</h1>}></Route>
+          </Routes>
+        </div>
+
+
       </div>
     </>
   )
